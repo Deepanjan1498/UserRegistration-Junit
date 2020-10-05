3cb9ccf3cb6708cd8189a8b72017e8bdcddf34b2
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-05 17:29:19 +0530

    [Deepanjan]Add . uc5 is solved.

--- src/main/java/org/bridgelabz/userregjunit/UserReg.java
+++ src/main/java/org/bridgelabz/userregjunit/UserReg.java
@@ -8,24 +8,24 @@ import java.util.regex.Pattern;
 public class UserReg {
 
 	public static void main(String[] args) {
-		List<String> Mobile = new ArrayList<String>();
+		List<String> password = new ArrayList<String>();
 
-		Mobile.add("456789");
-		Mobile.add("91 7896354128");
-		Mobile.add("00 1234567896");
-		Mobile.add("103 2314569875");
-		Mobile.add("1 1234567895");
-		Mobile.add("15 12345685");
-		Mobile.add("15  1234568579");
-		Mobile.add("15 0234568579");
-		Mobile.add("15 12345685793");
+		password.add("456789");
+		password.add("@#$%2345");
+		password.add("AsDfGhJkl");
+		password.add("!@#$%^&*()");
+		password.add("--");
+		password.add("asdfghjkl");
+		password.add("15@1234568579");
+		password.add("15 0234568579");
+		password.add("ab312345685793");
 
-		String regex = "^[1-9]([0-9]{1,2})? ([1-9][0-9]{9})$";
+		String regex = "^[~!@#$%^&*()+=\\w\\d.]{8,}+$";
 
 		Pattern pattern = Pattern.compile(regex);
 
-		for (String number : Mobile) {
-			Matcher matcher = pattern.matcher(number);
+		for (String pass : password) {
+			Matcher matcher = pattern.matcher(pass);
 			System.out.println(matcher.matches());
 		}
 	}
