4e3a98d507328a4421e831ccc604c4a28d0c76c5
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-05 17:24:08 +0530

    [Deepanjan]Add . uc4 is solved.

--- src/main/java/org/bridgelabz/userregjunit/UserReg.java
+++ src/main/java/org/bridgelabz/userregjunit/UserReg.java
@@ -8,37 +8,24 @@ import java.util.regex.Pattern;
 public class UserReg {
 
 	public static void main(String[] args) {
-		List<String> emailId = new ArrayList<String>();
+		List<String> Mobile = new ArrayList<String>();
 
-		emailId.add("+bc@yahoo.com");
-		emailId.add("abc-100@yahoo.com");
-		emailId.add("abc.100@yahoo.com");
-		emailId.add("abc111@abc.com");
-		emailId.add("abc-100@abc.net");
-		emailId.add("abc.100@abc.com.au");
-		emailId.add("abc@1.com");
-		emailId.add("abc@gmial.com.com");
-		emailId.add("abc+100@gmail.com");
-		emailId.add("abc-");
-		emailId.add(".abc@.com.my");
-		emailId.add("abc123@gmail.a");
-		emailId.add("abc123@.com");
-		emailId.add("abc123@.com.com");
-		emailId.add(".abc@abc.com");
-		emailId.add("abc()*@gmail.com");
-		emailId.add("abc@%*.com");
-		emailId.add("abc..2002@gmail.com");
-		emailId.add("abc.@gmail.com");
-		emailId.add("abc@abc@gmail.com");
-		emailId.add("abc@gmail.com.1a");
-		emailId.add("abc@gmail.com.au.au");
+		Mobile.add("456789");
+		Mobile.add("91 7896354128");
+		Mobile.add("00 1234567896");
+		Mobile.add("103 2314569875");
+		Mobile.add("1 1234567895");
+		Mobile.add("15 12345685");
+		Mobile.add("15  1234568579");
+		Mobile.add("15 0234568579");
+		Mobile.add("15 12345685793");
 
-		String regex = "^[A-Za-z0-9]+([-\\+\\.][A-Za-z0-9]+)?@([A-Za-z0-9-]+)\\.([A-Za-z]{2,3})(\\.[A-Za-z]{1,3})?$";
+		String regex = "^[1-9]([0-9]{1,2})? ([1-9][0-9]{9})$";
 
 		Pattern pattern = Pattern.compile(regex);
 
-		for (String email : emailId) {
-			Matcher matcher = pattern.matcher(email);
+		for (String number : Mobile) {
+			Matcher matcher = pattern.matcher(number);
 			System.out.println(matcher.matches());
 		}
 	}
